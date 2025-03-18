18f45cb13f16f8c8483508627b62b346a0b8ed81
Ryser <rysercooksey67@gmail.com>
2025-03-19 10:14:44 +1300

    The whole thing

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-print("Hello")
-print("hello me")
+def askname ():
+    persons_name = input("What is your name? ")
+    name_list.append(persons_name)
+def printname ():
+    print(f"The name is (name_list{0})")
+name_list = []
+askname()
+print()
