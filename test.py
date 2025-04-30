53a5b446e2f00bdb56a037d2e850149b1f0e6482
ryser <rysercooksey67@gmail.com>
2025-05-01 11:42:24 +1200

    making age reqiurement

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 input("What is your name: ")
+age=input("What is your age:")
+if age >= 18: 
+print("You are too old")
+if age =< 5:
+print("You are too young")
+
+
+
+
+
+
 print("Activity")
 print("Cultural immersion. This is for 5 days and is considered “easy” and costs $800. ")
 print("Kayaking and pancakes. This is for 3 days and is considered “moderate” and costs $ 400. ")
 print("Mountain biking. This is for 4 days and is considered “difficult” and costs $900. ")
+input("Choose your activity: ")
