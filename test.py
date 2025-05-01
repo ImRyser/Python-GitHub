4f1bbf6d7a0c9facbc9039672750dad11d1b489e
ryser <rysercooksey67@gmail.com>
2025-05-01 12:12:12 +1200

    age work

--- test.py
+++ test.py
@@ -1,15 +1,11 @@
-input("What is your name: ")
-age=input("What is your age:")
-if age >= 18: 
-print("You are too old")
-if age =< 5:
-print("You are too young")
-
-
-
-
-
-
+first_name = input("What is your name: ")
+current_age = int(input("What is your age: "))
+if current_age > 18:
+ print("You're too old")
+if current_age < 5:
+ print("You are too young")
+else:
+ print("You are able to go")
 print("Activity")
 print("Cultural immersion. This is for 5 days and is considered “easy” and costs $800. ")
 print("Kayaking and pancakes. This is for 3 days and is considered “moderate” and costs $ 400. ")
