ec976b0e298487d3fa68e25a541d980c97b2dda2
ryser <rysercooksey67@gmail.com>
2025-05-02 12:39:10 +1200

    updated writing

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ print("Activity")
 print("1. Cultural immersion. This is for 5 days and is considered “easy” and costs $800. ")
 print("2. Kayaking and pancakes. This is for 3 days and is considered “moderate” and costs $ 400. ")
 print("3. Mountain biking. This is for 4 days and is considered “difficult” and costs $900. ")
-place = input("Choose your activity: ")
+place = input("Choose the number of your activity: ")
 if place == "1":
  print(f"{first_name} age {current_age} has chosen Cultural immersion for $800")
 if place == "2":
