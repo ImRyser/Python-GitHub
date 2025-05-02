8581150de638e23d0bc337e0e9d6ea976af4ce44
ryser <rysercooksey67@gmail.com>
2025-05-02 12:34:02 +1200

    Updated program

--- test.py
+++ test.py
@@ -7,13 +7,13 @@ if current_age < 5:
 else:
  print("You are able to go")
 print("Activity")
-print("1.Cultural immersion. This is for 5 days and is considered “easy” and costs $800. ")
-print("2.Kayaking and pancakes. This is for 3 days and is considered “moderate” and costs $ 400. ")
-print("3.Mountain biking. This is for 4 days and is considered “difficult” and costs $900. ")
+print("1. Cultural immersion. This is for 5 days and is considered “easy” and costs $800. ")
+print("2. Kayaking and pancakes. This is for 3 days and is considered “moderate” and costs $ 400. ")
+print("3. Mountain biking. This is for 4 days and is considered “difficult” and costs $900. ")
 place = input("Choose your activity: ")
 if place == "1":
- print(f"{first_name} age {current_age} has chosen Cultural immersion.")
+ print(f"{first_name} age {current_age} has chosen Cultural immersion for $800")
 if place == "2":
- print(f"{first_name} age {current_age} has chosen Kayaking and pancakes.")
+ print(f"{first_name} age {current_age} has chosen Kayaking and pancakes for $400.")
 if place == "3":
- print(f"{first_name} age {current_age} has chosen mountain biking.")
+ print(f"{first_name} age {current_age} has chosen mountain biking for $900")
