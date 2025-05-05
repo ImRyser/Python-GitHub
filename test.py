3499d2ff5b1687b3432a7b9c7b49efc1e7a5fa14
ryser <rysercooksey67@gmail.com>
2025-05-06 09:23:59 +1200

    progress made

--- test.py
+++ test.py
@@ -1,19 +1,27 @@
 first_name = input("What is your name: ")
 current_age = int(input("What is your age: "))
-if current_age > 17:
+if current_age >= 18:
  print("You're too old")
-if current_age < 5:
+
+elif current_age <= 5:
  print("You are too young")
-if current_age == [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]:
- print("You are able to go:")
+
+else:
+ print("You are able to go")
  print("Activity")
  print("1. Cultural immersion. This is for 5 days and is considered “easy” and costs $800. ")
  print("2. Kayaking and pancakes. This is for 3 days and is considered “moderate” and costs $ 400. ")
  print("3. Mountain biking. This is for 4 days and is considered “difficult” and costs $900. ")
  place = input("Choose the number of your activity: ")
- if place == "1":
-  print(f"{first_name} age {current_age} has chosen Cultural immersion for $800")
- if place == "2":
-  print(f"{first_name} age {current_age} has chosen Kayaking and pancakes for $400.")
- if place == "3":
-  print(f"{first_name} age {current_age} has chosen mountain biking for $900")
+if place == 1:
+    print("You have chosen cultural immersion")
+elif place == 2:
+    print("You have chosen kayaking and pancakes")
+else:
+    print("you have chosen mountain biking")
+
+transport = input("Do you need transport? (cost is $80)")
+if transport == "yes":
+    print("$80 has been added to your cost")
+else:
+    print("We will see you there!")
