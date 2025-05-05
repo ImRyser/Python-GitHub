f6ef977c4d1068863f66915b0c105a066b3e8281
ryser <rysercooksey67@gmail.com>
2025-05-06 09:31:45 +1200

    half

--- test.py
+++ test.py
@@ -13,15 +13,19 @@ else:
  print("2. Kayaking and pancakes. This is for 3 days and is considered “moderate” and costs $ 400. ")
  print("3. Mountain biking. This is for 4 days and is considered “difficult” and costs $900. ")
  place = input("Choose the number of your activity: ")
-if place == 1:
-    print("You have chosen cultural immersion")
-elif place == 2:
-    print("You have chosen kayaking and pancakes")
-else:
-    print("you have chosen mountain biking")
-
-transport = input("Do you need transport? (cost is $80)")
-if transport == "yes":
-    print("$80 has been added to your cost")
-else:
-    print("We will see you there!")
+ if place == "1":
+     print("You have chosen cultural immersion")
+ elif place == "2":
+     print("You have chosen kayaking and pancakes")
+ else:
+     print("you have chosen mountain biking")
+ transport = input("Do you need transport? (cost is $80) ")
+ if transport == "yes":
+     print("$80 has been added to your cost")
+ else:
+     print("We will see you there!")
+      
+ print("Meal options")
+ print("1. Standard")
+ print("2. Vegan")
+ print("3. Vegetarian")
